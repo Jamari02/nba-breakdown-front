23aed28bd9fb0895e6450af3b3e179d6102a55aa
Jamari02 <castrojamari@gmail.com>
2023-06-05 06:51:57 -0400

    Almost completed

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# nba-breakdown-front
+# BBall Breakdown
+
+This is a web application called "BBall Breakdown" that allows users to explore basketball teams, players, and standings. The application retrieves data from a remote API and provides various functionalities to search and display information.
+
+## Features
+
+The BBall Breakdown application offers the following features:
+
+- Random Team: Clicking the "Random Team" button fetches a random basketball team from the API and displays its name, city, and code.
+
+- Random Player: Clicking the "Random Player" button fetches a random basketball player from the API and displays their name and college.
+
+- Get Team Records: Clicking the "Get Team Records" button retrieves the standings and records of basketball teams from the API and displays them. Clicking the button again hides the standings.
+
+- Search Team: Users can enter a search query in the "Find Team..." input field and click the "Search" button to find basketball teams matching the query. The application fetches the teams from the API and displays the matching results.
+
+- Search Player: Users can enter a search query in the "Find Player..." input field and click the "Search" button to find basketball players matching the query. The application fetches the players from the API and displays the matching results.
+
+## How to Use
+
+To use the BBall Breakdown application, follow these steps:
+
+1. Open the `index.html` file in a web browser.
+
+2. Use the provided buttons and input fields to interact with the application:
+
+   - Click "Random Team" to get a random basketball team.
+   - Click "Random Player" to get a random basketball player.
+   - Click "Get Team Records" to retrieve and display the standings and records of basketball teams.
+   - Enter a search query in the "Find Team..." input field and click "Search" to find basketball teams.
+   - Enter a search query in the "Find Player..." input field and click "Search" to find basketball players.
+
+3. The application will fetch data from the remote API and display the results based on your interactions.
+
+## Technologies Used
+
+The BBall Breakdown application is built using the following technologies:
+
+- HTML: Provides the structure and elements of the web page.
+- CSS: Adds styles and visual enhancements to the application.
+- JavaScript: Implements the functionality and logic of the application.
+- Fetch API: Retrieves data from the remote API.
+- REST API: Interacts with the remote API to fetch team, player, and standings data.
+
+## APIs Used
+
+The BBall Breakdown application fetches data from the following APIs:
+
+- Team API: Retrieves information about basketball teams, including name, city, and code.
+- Players API: Fetches data about basketball players, including name and college.
+- Standings API: Retrieves standings and records of basketball teams.
+
+## Disclaimer
+
+The data displayed in the BBall Breakdown application is sourced from a remote API and may be subject to change or inaccuracies. The application is not affiliated with the API provider and is intended for learning purposes only.
--- script.js
+++ script.js
@@ -1,40 +1,352 @@
-//Connection to backend 
-app.use(express.static('nba-breakdown'));
+// Buttons
+const randomTeam = document.getElementById('random-team').addEventListener('click', newTeam);
+const randomPlayer = document.getElementById('random-player').addEventListener('click', newPlayer);
+const records = document.getElementById('records').addEventListener('click', checkStandings);
+const grabPlayer = document.getElementById('searchFormPlayer').addEventListener('click', searchPlayer);
+const grabTeam = document.getElementById('searchFormTeam').addEventListener('submit', searchTeam);
+const hideStandingsButton = document.getElementById('hideStandings');
+hideStandingsButton.addEventListener('click', hideStandings);
 
-//Buttons
-const team = document.getElementById('find-team').addEventListener('click', searchTeam)
-const player = document.getElementById('find-player').addEventListener('click', searchPlayer)
-const standings = document.getElementById('standings').addEventListener('click', checkStandings)
 
-//Search Bar
-const searchBar = document.getElementById("searchForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevents the form from submitting and refreshing the page
-    performSearch();
+// Search Bar
+const teamSearchBar = document.getElementById("submitTeam").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevents the form from submitting and refreshing the page
+  
+});
+
+const playerSearchBar = document.getElementById("submitPlayer").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevents the form from submitting and refreshing the page
+  
+});
+
+// Data Grab
+async function getTeams() {
+  try {
+    const response = await fetch('https://nba-breakdown.herokuapp.com/api/team');
+    if (!response.ok) {
+      throw new Error('Failed to fetch teams');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function getPlayers() {
+  try {
+    const response = await fetch('https://nba-breakdown.herokuapp.com/api/players');
+    if (!response.ok) {
+      throw new Error('Failed to fetch players');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function getRecords() {
+  try {
+    const response = await fetch('https://nba-breakdown.herokuapp.com/api/standing');
+    if (!response.ok) {
+      throw new Error('Failed to fetch records');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+//Functions
+
+function searchTeam() {
+  let findTeam = document.getElementById("findTeam").value;
+  console.log("Searching for: " + findTeam);
+
+  getTeams()
+    .then(data => {
+      const foundTeams = data.filter(team => team.name.toLowerCase().includes(findTeam.toLowerCase()));
+      console.log(foundTeams);
+      displayTeams(foundTeams); // Pass the found teams to the displayTeams function
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function displayTeams(team) {
+  const teamsContainer = document.getElementById("teamsContainer");
+  teamsContainer.innerHTML = ""; // Clear previous results
+
+  const teamElement = document.createElement("div");
+  teamElement.textContent = `Name: ${team.name}, City: ${team.city}, Code: ${team.code}`;
+
+  // // Create a delete button
+  // const deleteButton = document.createElement("button");
+  // deleteButton.textContent = "Delete";
+  // deleteButton.addEventListener("click", function () {
+  //   deleteTeam(team.id); // Call the deleteTeam function with the team id
+  // });
+
+  // // Append the delete button to the team element
+  // teamElement.appendChild(deleteButton);
+
+  teamsContainer.appendChild(teamElement);
+}
+
+getTeams()
+  .then(data => {
+    displayTeams(data);
+  })
+  .catch(error => {
+    console.error(error);
   });
 
+function searchPlayer() {
+  let findPlayer = document.getElementById("findPlayer").value;
+  console.log("Searching for: " + findPlayer);
 
+  getPlayers()
+    .then(data => {
+      const foundPlayers = data.filter(player => {
+        const fullName = player.firstname + " " + player.lastname;
+        return fullName.toLowerCase().includes(findPlayer.toLowerCase());
+      });
 
+      if (foundPlayers.length > 0) {
+        console.log(foundPlayers);
+        displayPlayers(foundPlayers); // Pass the found players to the displayPlayers function
+      } else {
+        console.log("No players found.");
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
-  //Functions
+function displayPlayers(player) {
+  const playersContainer = document.getElementById("playersContainer");
+  playersContainer.innerHTML = ""; // Clear previous results
 
-   function searchTeam(){
+  const playerElement = document.createElement("div");
+  playerElement.textContent = `Name: ${player.firstname} ${player.lastname}, College: ${player.college}`;
 
-   }
+  playersContainer.appendChild(playerElement);
+}
+getPlayers()
+  .then(data => {
+    displayPlayers(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
-   function searchPlayer(){
+  let standingsVisible = false;
 
-   }
-
-   function checkStandings(){
-
-   }
-
-
-
-  function performSearch() {
-    let searchTerm = document.getElementById("searchInput").value;
-    // Perform your search logic here
-    // You can use AJAX requests or search algorithms to fetch and display results
-    console.log("Searching for: " + searchTerm);
-    alert("Searching for: " + searchTerm);
+  async function checkStandings() {
+    try {
+      const standings = await getRecords();
+  
+      const filteredStandings = standings.map(standing => {
+        return {
+          season: standing.season,
+          team: standing.team,
+          win: standing.win,
+          loss: standing.loss
+        };
+      });
+  
+      if (standingsVisible) {
+        hideStandings();
+      } else {
+        displayStandings(filteredStandings);
+        standingsVisible = true;
+      }
+  
+      // Show the "hideStandings" button
+      hideStandingsButton.style.display = 'block';
+    } catch (error) {
+      console.error(error);
+    }
   }
+  
+  function hideStandings() {
+    const standingsContainer = document.getElementById('standingsContainer');
+    standingsContainer.style.display = 'none';
+  
+    hideStandingsButton.style.display = 'none';
+  
+    standingsVisible = false;
+  }
+  
+  function displayStandings(standings) {
+    const standingsContainer = document.getElementById('standingsContainer');
+    standingsContainer.innerHTML = ''; // Clear previous results
+  
+    standings.forEach(standing => {
+      // Create a DOM element to display the desired fields
+      const standingElement = document.createElement('div');
+      standingElement.textContent = `Season: ${standing.season}, Team: ${standing.team}, Win: ${standing.win}, Loss: ${standing.loss}`;
+  
+      standingsContainer.appendChild(standingElement);
+    });
+  }
+  
+function newTeam() {
+  getTeams()
+    .then(data => {
+      // Get the total number of teams
+      const totalTeams = data.length;
+
+      // Generate a random index between 0 and totalTeams-1
+      const randomTeamIndex = Math.floor(Math.random() * totalTeams);
+
+      // Get the random team
+      const randomTeam = data[randomTeamIndex];
+
+      // Display the random team
+      displayTeams(randomTeam);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function newPlayer() {
+  getPlayers()
+    .then(data => {
+      // Get the total number of players
+      const totalPlayers = data.length;
+
+      // Generate a random index between 0 and totalPlayers-1
+      const randomPlayerIndex = Math.floor(Math.random() * totalPlayers);
+
+      // Get the random player
+      const randomPlayer = data[randomPlayerIndex];
+
+      // Display the random player
+      displayPlayers(randomPlayer);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function createTeam(name, city, code) {
+  const newTeam = {
+    name: name,
+    city: city,
+    code: code
+  };
+
+  fetch('https://nba-breakdown.herokuapp.com/api/team', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newTeam),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the created team data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function updateTeam(id, updatedTeam) {
+  fetch(`https://nba-breakdown.herokuapp.com/api/team/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedTeam),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the updated team data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function deleteTeam(id) {
+  fetch(`https://nba-breakdown.herokuapp.com/api/team/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the deleted team data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function createPlayer(firstname, lastname, college) {
+  const newPlayer = {
+    firstname: firstname,
+    lastname: lastname,
+    college: college
+  };
+
+  fetch('https://nba-breakdown.herokuapp.com/api/players', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPlayer),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the created player data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function updatePlayer(id, updatedPlayer) {
+  fetch(`https://nba-breakdown.herokuapp.com/api/players/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedPlayer),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the updated player data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function deletePlayer(id) {
+  fetch(`https://nba-breakdown.herokuapp.com/api/players/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the deleted player data here
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
